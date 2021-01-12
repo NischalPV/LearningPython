f29ac868027bd92a066d5fb6f4037801f0097c22
Nischal Varasada <nischal.varasada@xpertnest.com>
2021-01-12 10:54:02 +0530

    Initial Commit

---
+++ math.py
@@ -0,0 +1,11 @@
+x = int(input("enter the value of x: "))
+y= int(input("enter the value of y: "))
+print("Adding Numbers")
+z = x + y
+print("Sum =")
+print(z)
+
+print("Dividing Numbers")
+z = x/y
+print("Quotient =")
+print(z)
---
+++ string.py
@@ -0,0 +1,6 @@
+# firstName = "Payal"
+# lastName = "Gol"
+firstName=input("enter your first Name: ")
+lastName=input("enter your last Name: ")
+print(firstName+lastName)
+print("Hello, " + firstName + " " + lastName)
